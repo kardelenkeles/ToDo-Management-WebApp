7b6263fed13291686f4d6baf6c064470bbb3a216
kardelenkeles <kar_di44@hotmail.com>
2023-05-08 19:21:11 +0300

    toDoService enhanced

--- src/main/java/todo/ToDoController.java
+++ src/main/java/todo/ToDoController.java
@@ -1,12 +1,18 @@
 package todo;
 
+import org.springframework.cglib.core.Local;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
+@SessionAttributes("name")
 public class ToDoController {
 
     public ToDoController(ToDoService toDoService) {
@@ -17,9 +23,18 @@ public class ToDoController {
 
     @RequestMapping("list-todos")
     public String listTodos(ModelMap model){
-        List<ToDoPage> todos = toDoService.findByUsername("kardelenkeles");
+        List<ToDoPage> todos = ToDoService.findByUsername("kardelenkeles");
         model.addAttribute("todos", todos);
         return "listTodos";
     }
-
+    @RequestMapping(value = "add-todo", method = RequestMethod.GET)
+    public String showTodoPage(){
+        return "todo";
+    }
+    @RequestMapping(value = "add-todo", method = RequestMethod.POST)
+    public String addNewTodo(@RequestParam String desc, ModelMap model){
+        String username = (String) model.get("name");
+        toDoService.addTodo(username, desc, LocalDate.now().plusYears(1),false);
+        return "redirect:list-todos";
+    }
 }
--- src/main/java/todo/ToDoService.java
+++ src/main/java/todo/ToDoService.java
@@ -9,14 +9,19 @@ import java.util.List;
 @Service
 public class ToDoService {
     private static List<ToDoPage> todos = new ArrayList<>();
+    private static int todosCount = 0;
     static {
-        todos.add(new ToDoPage(1, "keles", "Istanbul",
+        todos.add(new ToDoPage(++todosCount, "kardelenkeles", "Istanbul",
                 LocalDate.now().plusYears(1), false));
-        todos.add(new ToDoPage(2, "keles", "Istanbul",
+        todos.add(new ToDoPage(++todosCount, "kardelenkeles", "Istanbul",
                 LocalDate.now().plusYears(2), false));
     }
 
     public static List<ToDoPage> findByUsername(String username) {
         return todos;
+    }
+    public void addTodo(String username, String desc, LocalDate targetDate, boolean done){
+        ToDoPage todo= new ToDoPage(++todosCount, username, desc, targetDate, done);
+        todos.add(todo);
     }
 }
